bd9f5d3cad53b1adbdeb7d7b1e8d9348dea612a4
Amanda Stanton <44312668+amandastantonKC@users.noreply.github.com>
2019-02-24 15:08:52 -0600

    update
    added the request to show concert info

--- liri.js
+++ liri.js
@@ -34,3 +34,29 @@ function userInputs(userOption, inputParameter) {
 
     }
 }
+
+function showConcertInfo(inputParameter){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
+    request(queryUrl, function(error, response, body) {
+    
+        // If the request is successful
+    if (!error && response.statusCode === 200) {
+        var concerts = JSON.parse(body);
+        for (var i = 0; i < concerts.length; i++) {  
+            console.log(","); 
+            fs.appendFileSync("log.txt", "\n");//Append in log.txt file
+            console.log(i);
+            fs.appendFileSync("log.txt", i + "\n");
+            console.log("Name of the Venue: " + concerts[i].venue.name);
+            fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
+            console.log("Venue Location: " +  concerts[i].venue.city);
+            fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
+            console.log("Date of the Event: " +  concerts[i].datetime);
+            fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
+            console.log("");
+            fs.appendFileSync("log.txt", "" + "\n");
+        }
+    } else{
+      console.log('Error');
+    }
+});}
