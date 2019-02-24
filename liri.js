66bce45f4a634767e80aad0d81b033bcb760ffb2
Amanda Stanton <44312668+amandastantonKC@users.noreply.github.com>
2019-02-24 14:46:15 -0600

    update liri.js
    updated dependencies, added switch statement

--- liri.js
+++ liri.js
@@ -1,10 +1,36 @@
-var keys = require("./keys.js");
+require("dotenv").config();
 
-var request = require ('request');
-var twitter = require ('twitter');
-var spotify = require ('node-spotify-api');
-var fs = require ('fs');
-var client = new twitter (keys.Twitter.keys);
-var input = process.argv;
-var action = input[2];
-var inputs = input[3];
+//dependencies
+var keys = require("./keys.js");
+var request = require('request');
+var fs = require('fs');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+//variables to capture user inputs
+var userOption = process.argv[2];
+var inputParameter = process.argv[3];
+
+userInputs(userOption, inputParameter);
+
+function userInputs(userOption, inputParameter) {
+    switch (userOption) {
+        case 'concert-this':
+            showConcertInfo(inputParameter);
+            break;
+
+        case ' spotify-this-song':
+            showSongInfo(inputParameter);
+            break;
+
+        case 'do-what-it-says':
+            showSomeInfo();
+            break;
+
+        default:
+
+            console.log("invalid option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says");
+
+
+    }
+}
