fae158b5c3093cabf106a01817fabd308bc63087
Amanda Stanton <44312668+amandastantonKC@users.noreply.github.com>
2019-02-24 17:40:01 -0600

    updated file
    corrected file

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
-(exports.spotify) = {
-    id: 'a016fe0aa6b84479bb76d71a8596dbc7',
-    secret: '6e467b8ec4fd4365891fad59a17dcc0f'
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
